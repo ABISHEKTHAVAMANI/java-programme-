7c127886727add390b16286f2a32a40812e3973e
ABISHEKTHAVAMANI <abishekt.23ece@kongu.edu>
2026-04-29 12:21:51 +0530

    java.abi

---
+++ MyClass.java
@@ -0,0 +1,11 @@
+ class MyClass {
+    public static void main(String[] args) {
+        int age = 18;
+
+        if (age >= 18) {
+            System.out.println("You are an adult");
+        } else {
+            System.out.println("You are a minor");
+        }
+    }
+}
